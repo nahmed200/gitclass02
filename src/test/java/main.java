33424399628f43182ed1188bd548736b23aa5f29
nahmed200 <billahmed93@gmail.com>
2025-09-01 22:49:29 -0400

    third commit

--- src/test/java/main.java
+++ src/test/java/main.java
@@ -1,13 +1,10 @@
 public class main {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println("This is the project version 2");
-        System.out.println("Hello World");
-        System.out.println("Hello World");
-        System.out.println("Hello World");
-        System.out.println("Hello World");
+        System.out.println("This is the project version ");
+
 
 
     }
 }
-// done with version two
+// done with version three
