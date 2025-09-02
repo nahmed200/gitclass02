ce7dc1a2f933828868a2ce14ad26a850e1db2bb3
nahmed200 <billahmed93@gmail.com>
2025-09-01 22:33:32 -0400

    first commit

---
+++ src/test/java/main.java
@@ -0,0 +1,8 @@
+public class main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("This is the project version 1");
+
+    }
+}
+// done with version one
