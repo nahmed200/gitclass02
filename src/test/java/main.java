cc3984656a19695331ccf87d2299019d82a1fd8f
nahmed200 <billahmed93@gmail.com>
2025-09-08 14:24:10 -0400

    person2commit

--- src/test/java/main.java
+++ src/test/java/main.java
@@ -1,7 +1,7 @@
 public class main {
     public static void main(String[] args) {
 
-        System.out.println("This is the project version 5");
+        System.out.println("This is the project version 6 made by person 2");
 
 
 
